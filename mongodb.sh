21eb283038563dbeec2280f24ec57c616248ff31
Zaheer <thisiszaheer.786@gmail.com>
2023-05-28 08:40:02 +0530

    Roboshop MongoDB Script

--- mongodb.sh
+++ mongodb.sh
@@ -4,7 +4,8 @@ cp mongodb.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
 echo -e "\e[36mInstalling MongoDB Server\e[0m"
 yum install mongodb-org -y &>>/tmp/roboshop.log
 
-#create local host
+echo -e "\e[33mUpdate IP Address\e[0m"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 
 echo -e "\e[36mStart MongoDB\e[0m"
 systemctl enable mongod  &>>/tmp/roboshop.log
