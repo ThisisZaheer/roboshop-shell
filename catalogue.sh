902aa2115a2e22ede5e3b73329903e1985ffde66
Zaheer <thisiszaheer.786@gmail.com>
2023-05-28 09:34:45 +0530

    Roboshop Catalogue Script

--- catalogue.sh
+++ catalogue.sh
@@ -1,8 +1,8 @@
-echo -e "\e[32mConfiguration files\e[om"
+echo -e "\e[32mConfiguration files\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/roboshop.log
-echo -e "\e[32mInstalling NodeJS\e[om"
+echo -e "\e[32mInstalling NodeJS\e[0m"
 yum install nodejs -y &>>/tmp/roboshop.log
-echo -e "\e[32mAdding User\e[om"
+echo -e "\e[32mAdding User\e[0m"
 useradd roboshop &>>/tmp/roboshop.log
 echo -e "\e[32mRemoving default content\e[0m"
 rm -rf /app &>>/tmp/roboshop.log
@@ -10,23 +10,23 @@ mkdir /app
 echo -e "\e[32mDownloading the catalogue content"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>/tmp/roboshop.log
 cd /app
-echo -e "\e[Extract the catalogue content\e[om"
+echo -e "\e[Extract the catalogue content\e[0m"
 unzip /tmp/catalogue.zip &>>/tmp/roboshop.log
 cd /app
-echo -e "\e[32mInstalling NodeJS Depenencies\e[om"
+echo -e "\e[32mInstalling NodeJS Depenencies\e[0m"
 npm install &>>/tmp/roboshop.log
-echo -e "\e[32mCopy Service files\e[om"
-cp catalogue.service /etc/systemd/system/catalogue.service &>>/tmp/roboshop.log
+echo -e "\e[32mCopy Service files\e[0m"
+cp /root/roboshop-shell catalogue.service /etc/systemd/system/catalogue.service &>>/tmp/roboshop.log
 #/etc/systemd/system/catalogue.service
-echo -e "\e[32mRelaod files\e[om"
+echo -e "\e[32mRelaod files\e[0m"
 systemctl daemon-reload &>>/tmp/roboshop.log
-echo -e "\e[32mStart the Server\e[om"
+echo -e "\e[32mStart the Server\e[0m"
 systemctl enable catalogue &>>/tmp/roboshop.log
 systemctl restart catalogue &>>/tmp/roboshop.log
-echo -e "\e[32mCopy the MongoDB Repo Service\e[om"
-cp mongodb.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
+echo -e "\e[32mCopy the MongoDB Repo Service\e[0m"
+cp /root/roboshop-shell mongodb.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
 #/etc/yum.repos.d/mongo.repo
-echo -e "\e[32mInstall Mongodb Server\e[om"
+echo -e "\e[32mInstall Mongodb Server\e[0m"
 yum install mongodb-org-shell -y &>>/tmp/roboshop.log
-echo -e "\e[32mSchema files load\e[om"
+echo -e "\e[32mSchema files load\e[0m"
 mongo --host mongodb-dev.thisiszaheer.online </app/schema/catalogue.js &>>/tmp/roboshop.log
--- frontend.sh
+++ frontend.sh
@@ -11,7 +11,7 @@ echo -e "\e[34mExtracting the Frontend Content\e[0m"
 cd /usr/share/nginx/html &>>/tmp/roboshop.log
 unzip /tmp/frontend.zip &>>/tmp/roboshop.log
 
-cp roboshop.conf /etc/nginx/default.d/roboshop.conf &>>/tmp/roboshop.log
+cp /root/roboshop-shell roboshop.conf /etc/nginx/default.d/roboshop.conf &>>/tmp/roboshop.log
 
 echo -e "\e[35mStarting Nginx Server\e[0m"
 systemctl enable nginx &>>/tmp/roboshop.log
