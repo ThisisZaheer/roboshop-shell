fabdca59ea5d92778e16e78707241e246b737313
Zaheer <thisiszaheer.786@gmail.com>
2023-05-29 08:38:15 +0530

    Roboshop Redis Script

--- redis.sh
+++ redis.sh
@@ -1,11 +1,15 @@
 echo -e "\e[33mInstalling Redis Repo File\e[0m"
 yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>/tmp/roboshop.log
+
 echo -e "\e[33mEnabling Redis\e[0m"
 yum module enable redis:remi-6.2 -y &>>/tmp/roboshop.log
+
 echo -e "\e[33mInstall Redis server\e[0m"
 yum install redis -y &>>/tmp/roboshop.log
+
 echo -e "\e[33mupdating IP Address\e[0m"
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf &>>/tmp/roboshop.log
+
 echo -e "\e[33mStart Redis Directory\e[0m"
 systemctl enable redis &>>/tmp/roboshop.logk
 systemctl start redis &>>/tmp/roboshop.log
