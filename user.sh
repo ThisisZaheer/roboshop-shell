079a638cac0b80ea82a80fc73fec0974a5e726b3
Zaheer <thisiszaheer.786@gmail.com>
2023-05-29 09:03:07 +0530

    Roboshop User Script

--- user.sh
+++ user.sh
@@ -11,34 +11,32 @@ echo -e "\e[32mRemoving default content\e[0m"
 rm -rf /app &>>/tmp/roboshop.log
 mkdir /app
 
-echo -e "\e[32mDownloading the catalogue content"
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>/tmp/roboshop.log
+echo -e "\e[32mDownloading the user content"
+curl -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip &>>/tmp/roboshop.log
 cd /app
 
-echo -e "\e[36mExtract the catalogue content\e[0m"
-unzip /tmp/catalogue.zip &>>/tmp/roboshop.log
+echo -e "\e[36mExtract the user content\e[0m"
+unzip /tmp/user.zip &>>/tmp/roboshop.log
 cd /app
 
 echo -e "\e[32mInstalling NodeJS Depenencies\e[0m"
 npm install &>>/tmp/roboshop.log
 
 echo -e "\e[32mCopy Service files\e[0m"
-cp /root/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service &>>/tmp/roboshop.log
-
+cp /root/roboshop-shell/user.service /etc/systemd/system/user.service &>>/tmp/roboshop.log
 
 echo -e "\e[32mReload files\e[0m"
 systemctl daemon-reload &>>/tmp/roboshop.log
 
 echo -e "\e[32mStart the Server\e[0m"
-systemctl enable catalogue &>>/tmp/roboshop.log
-systemctl restart catalogue &>>/tmp/roboshop.log
+systemctl enable user &>>/tmp/roboshop.log
+systemctl restart user &>>/tmp/roboshop.log
 
 echo -e "\e[32mCopy the MongoDB Repo Service\e[0m"
 cp /root/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
-
 
 echo -e "\e[32mInstall Mongodb Server\e[0m"
 yum install mongodb-org-shell -y &>>/tmp/roboshop.log
 
 echo -e "\e[32mSchema files load\e[0m"
-mongo --host mongodb-dev.thisiszaheer.online </app/schema/catalogue.js &>>/tmp/roboshop.log
+mongo --host mongodb-dev.thisiszaheer.online </app/schema/user.js &>>/tmp/roboshop.log
