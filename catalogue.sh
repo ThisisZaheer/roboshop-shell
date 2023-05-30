f2c86e28b27c3e38e18c19708c0c97ca0ea3db78
Zaheer <thisiszaheer.786@gmail.com>
2023-05-30 08:09:10 +0530

    Catalogue script

--- catalogue.sh
+++ catalogue.sh
@@ -1,3 +1,6 @@
+S{component} = catalogue
+
+
 echo -e "\e[32mConfiguration files\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/roboshop.log
 
@@ -11,27 +14,27 @@ echo -e "\e[32mRemoving default content\e[0m"
 rm -rf /app &>>/tmp/roboshop.log
 mkdir /app
 
-echo -e "\e[32mDownloading the catalogue content"
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>/tmp/roboshop.log
+echo -e "\e[32mDownloading the ${component} content"
+curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>/tmp/roboshop.log
 cd /app
 
-echo -e "\e[36mExtract the catalogue content\e[0m"
-unzip /tmp/catalogue.zip &>>/tmp/roboshop.log
+echo -e "\e[36mExtract the ${component} content\e[0m"
+unzip /tmp/${component}.zip &>>/tmp/roboshop.log
 cd /app
 
 echo -e "\e[32mInstalling NodeJS Depenencies\e[0m"
 npm install &>>/tmp/roboshop.log
 
 echo -e "\e[32mCopy Service files\e[0m"
-cp /root/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service
+cp /root/roboshop-shell/${component}.service /etc/systemd/system/${component}.service
 
 
 echo -e "\e[32mReload files\e[0m"
 systemctl daemon-reload &>>/tmp/roboshop.log
 
-echo -e "\e[32mStart the Server\e[0m"
-systemctl enable catalogue &>>/tmp/roboshop.log
-systemctl restart catalogue &>>/tmp/roboshop.log
+echo -e "\e[32mStart the ${component} Server\e[0m"
+systemctl enable ${component} &>>/tmp/roboshop.log
+systemctl restart ${component} &>>/tmp/roboshop.log
 
 echo -e "\e[32mCopy the MongoDB Repo Service\e[0m"
 cp /root/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongo.repo
@@ -40,4 +43,4 @@ echo -e "\e[32mInstall Mongodb Server\e[0m"
 yum install mongodb-org-shell -y &>>/tmp/roboshop.log
 
 echo -e "\e[32mSchema files load\e[0m"
-mongo --host mongodb-dev.thisiszaheer.online </app/schema/catalogue.js &>>/tmp/roboshop.log
+mongo --host mongodb-dev.thisiszaheer.online </app/schema/${component}.js &>>/tmp/roboshop.log
