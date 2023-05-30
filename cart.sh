a722157dc29cdd25c5166db5bfd2ef1321fe2156
Zaheer <thisiszaheer.786@gmail.com>
2023-05-30 09:08:07 +0530

    Cart script

--- cart.sh
+++ cart.sh
@@ -1,25 +1,31 @@
 source common.sh
-echo -e "\e[36m NodeJS Repo Files\e[0m"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/roboshop.log
-echo -e "\e[36m Installing NodeJS\e[0m"
-yum install nodejs -y &>>/tmp/roboshop.log
-echo -e "\e[36m Adding Cart User\e[0m"
-useradd roboshop &>>/tmp/roboshop.log
-echo -e "\e[36m Removing Any Default Content is Existed\e[0m"
-rm -rf /app &>>/tmp/roboshop.log
-mkdir /app
-echo -e "\e[36m Downloading the Cart Service files\e[0m"
-curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip &>>/tmp/roboshop.log
-cd /app
-echo -e "\e[36m Extracting the Cart Files\e[0m"
-unzip /tmp/cart.zip &>>/tmp/roboshop.log
-cd /app
-echo -e "\e[36m Installing NodeJS Dependency\e[0m"
-npm install &>>/tmp/roboshop.log
-echo -e "\e[36m Copying Cart Service\e[0m"
-cp /root/roboshop-shell/cart.service /etc/systemd/system/cart.service
-echo -e "\e[36m Reloading Cart Service\e[0m"
-systemctl daemon-reload &>>/tmp/roboshop.log
-echo -e "\e[36m Starting Cart Service\e[0m"
-systemctl enable cart &>>/tmp/roboshop.log
-systemctl restart cart &>>/tmp/roboshop.log
+component=cart
+color="\e[36m"
+nocolor="\e[0m"
+log_file="/tmp/roboshop.log"
+app_path="/app"
+
+echo -e "${color} NodeJS Repo Files${nocolor}"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
+echo -e "${color} Installing NodeJS${nocolor}"
+yum install nodejs -y &>>${log_file}
+echo -e "${color} Adding ${component} User${nocolor}"
+useradd roboshop &>>${log_file}
+echo -e "${color} Removing Any Default Content is Existed${nocolor}"
+rm -rf ${app_path} &>>${log_file}
+mkdir ${app_path}
+echo -e "${color} Downloading the ${component} Service files${nocolor}"
+curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log_file}
+cd ${app_path}
+echo -e "${color} Extracting the ${component} Files${nocolor}"
+unzip /tmp/${component}.zip &>>${log_file}
+cd ${app_path}
+echo -e "${color} Installing NodeJS Dependency${nocolor}"
+npm install &>>${log_file}
+echo -e "${color} Copying ${component} Service${nocolor}"
+cp /root/roboshop-shell/${component}.service /etc/systemd/system/${component}.service
+echo -e "${color} Reloading ${component} Service${nocolor}"
+systemctl daemon-reload &>>${log_file}
+echo -e "${color} Starting ${component} Service${nocolor}"
+systemctl enable ${component} &>>${log_file}
+systemctl restart ${component} &>>${log_file}
