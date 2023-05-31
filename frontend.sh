e2de77479654e1140f33d3358c14b391935ece84
Zaheer <thisiszaheer.786@gmail.com>
2023-05-31 13:01:00 +0530

    Doing Automation, with automation task becoming easy

--- frontend.sh
+++ frontend.sh
@@ -1,19 +1,19 @@
-echo -e "\e[31mInstalling Nginx Server\e[0m"
+echo -e "${color}Installing Nginx Server${nocolor}"
 yum install nginx -y &>>/tmp/roboshop.log
 
-echo -e "\e[32mRemoving Default Content on Server\e[0m"
+echo -e "${color}Removing Default Content on Server${nocolor}"
 rm -rf /usr/share/nginx/html/* &>>/tmp/roboshop.log
 
-echo -e "\e[33mDownloading Frontend Content\e[0m"
+echo -e "${color}Downloading Frontend Content${nocolor}"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>/tmp/roboshop.log
 
-echo -e "\e[34mExtracting the Frontend Content\e[0m"
+echo -e "${color}Extracting the Frontend Content${nocolor}"
 cd /usr/share/nginx/html &>>/tmp/roboshop.log
 unzip /tmp/frontend.zip &>>/tmp/roboshop.log
 
-echo -e "\e[36mUpdate Config Files\e[0m"
+echo -e "${color}Update Config Files${nocolor}"
 cp /root/roboshop-shell/roboshop.conf /etc/nginx/default.d/roboshop.conf
 
-echo -e "\e[35mStarting Nginx Server\e[0m"
+echo -e "${color}Starting Nginx Server${nocolor}"
 systemctl enable nginx &>>/tmp/roboshop.log
 systemctl restart nginx &>>/tmp/roboshop.log
